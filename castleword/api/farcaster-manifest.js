74c101bd3d35b3d69292335ff4452f58ab1c4583
airdrpkingg <josesancheziglesias21@gmail.com>
2026-03-15 00:36:33 +0100

    Update farcaster-manifest.js

--- castleword/api/farcaster-manifest.js
+++ castleword/api/farcaster-manifest.js
@@ -2,7 +2,11 @@ module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Content-Type', 'application/json');
   res.status(200).json({
-    accountAssociation: { header: "", payload: "", signature: "" },
+    accountAssociation: {
+      header: "eyJmaWQiOjI1Mzk4MSwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDAyMTc0NDJiMTdlYjBkQUQ4MmI1NUQ5NjZBYzQwN2YxMkZiOEQxMEUifQ",
+      payload: "eyJkb21haW4iOiJjYXN0bGV3b3JkYWlyZHJwa2luZy52ZXJjZWwuYXBwIn0",
+      signature: "tU4nSIj6u1ED8+pNDXdvoRUHQQmCBHO3z7PDDahKSvlIOp7Mdg1OkFE+iTt9xjvkLn+N+HpoMdDRShaVd5fq0Bs="
+    },
     miniapp: {
       version: "1",
       name: "CastleWord",
