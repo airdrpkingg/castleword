ff4d927b18409809679c568807211b2e551e3556
airdrpkingg <josesancheziglesias21@gmail.com>
2026-03-15 00:03:51 +0100

    Update farcaster-manifest.js

--- castleword/api/farcaster-manifest.js
+++ castleword/api/farcaster-manifest.js
@@ -1,4 +1,4 @@
-module.exports = function(req, res) {
+module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Content-Type', 'application/json');
   res.status(200).json({
